7559174485af3142a65a9436f64fbdd6a42fbf20
ekPo2 <eiko.115@mail.com>
2025-07-21 09:17:16 +0900

    add lesson14's new files

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require('mongodb');
-const uri = '****************';
+const uri = 'mongodb+srv://eiko:eikoeiko@cluster0.a1agogx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
@@ -11,10 +11,9 @@ router.get('/', async (req, res) => {
     const database = client.db('notes');
     const notes = database.collection('notes');
 
-    // idが1のドキュメントを取得
+    // // idが1のドキュメントを取得
     const query = { id: 2 };
     const note = await notes.findOne(query);
-    
     
     res.json(note);
 }) 
